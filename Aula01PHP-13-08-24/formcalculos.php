0ff0b6edc96c94cc9477175e4ab4bf5c209381f3
Yago-LDT <129117046+Yago-LDT@users.noreply.github.com>
2024-08-19 21:00:02 -0300

    Create formcalculos.php

---
+++ Aula01PHP-13-08-24/formcalculos.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action="recebecalculos.php">
+
+    <p><input type="number" name="valor1" size="4"></p>
+    <p><input type="submit" value="calcular"></p>
+    
+</form>
+</body>
+</html>
