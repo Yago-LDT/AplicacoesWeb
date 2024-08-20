ac52c33184bab04b588b6a8a40b6b4ddae89e4d4
Yago-LDT <129117046+Yago-LDT@users.noreply.github.com>
2024-08-19 21:00:50 -0300

    Create recebecalculos.php

---
+++ Aula01PHP-13-08-24/recebecalculos.php
@@ -0,0 +1,24 @@
+<?php
+$v= $_POST['valor1'];
+    for($cont=1;$cont<$v;$cont++){
+
+    echo ('<br>Valor digitado= ' .$cont );
+
+    }
+
+
+
+/*if ($v >20){
+    echo ('Valor digitado é maior que vinte= ' .$v );
+}
+
+else if ($v <20){
+    echo ('Valor digitado é menor que vinte= ' .$v );
+}
+
+else {
+    echo ('Valor digitado é igual a vinte= ' .$v );
+
+}
+*/
+?>
