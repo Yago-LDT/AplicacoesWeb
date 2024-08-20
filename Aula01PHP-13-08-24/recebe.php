ae86c8b8822b0bd7b5c9f907358ee032f2aa1505
Yago-LDT <129117046+Yago-LDT@users.noreply.github.com>
2024-08-19 21:00:26 -0300

    Create recebe.php

---
+++ Aula01PHP-13-08-24/recebe.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+</head>
+<body bgcolor="orange">
+    <?php
+    $_vnm =$_POST['nm'];
+    echo ('Bom dia, programando com PHP');
+    echo('<br> Nome digitado=' .$_vnm );    
+    ?>
+</body>
+</html>
