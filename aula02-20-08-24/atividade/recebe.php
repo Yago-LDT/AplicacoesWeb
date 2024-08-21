b71f69dd12ecee7e3a276cdeeca750d06ac74b25
Yago-LDT <129117046+Yago-LDT@users.noreply.github.com>
2024-08-21 12:22:46 -0300

    Update recebe.php

--- aula02-20-08-24/atividade/recebe.php
+++ aula02-20-08-24/atividade/recebe.php
@@ -1,26 +1,62 @@
 <html>
     <head>
+        <title>Valor das parcelas</title>
+  <meta charset="UTF-8"/>
+   <link rel="stylesheet" type="text/css" href="estilo.css"/>
+</head>
+<body>
         <div>
             <table id="table">
                 <?php
 $v=$_POST['valor'];
 $v1=$_POST['parcelas'];
 
-if($v1 >1 && <=3){
 
-    echo "Valor da compra: ",$v+($v*0,05),;
+if($v1 > 1){
+    echo "Valor da compra: ",$v, "<br>Quantidade de parcelas: ",$v1, "<br>Valor total da compra: ".$v+($v*0.05);
+
+    for($cont=1;$cont <= $v1; $cont++){
+        if($cont%2==0){
+            $class="clara";
+           }
+           else{
+            $class="escura";
+           }
+           $vtotal=$v+($v*0.05);
+            echo "<tr class=",$class,"><td>",($v+($v*0.05))/$v1,"</td></tr>"; 		   
+         }
     }
 
-    else if($v1>3){
+    else if($v1 > 3){
+    echo "Valor da compra: ",$v, "<br>Quantidade de parcelas: ",$v1, "<br>Valor total da compra: ".$v+($v*0.1);
 
-    echo "Valor da compra: ",$v+($v*0,1),;
+    for($cont=1;$cont <= $v1; $cont++){
+        if($cont%2==0){
+            $class="clara";
+           }
+           else{
+            $class="escura";
+           }
+           $vtotal=$v+($v*0.05);
+            echo "<tr class=",$class,"><td>",($v+($v*0.1))/$v1,"</td></tr>"; 		   
+         }
     }
 
-else {
-    echo "Valor da compra: ",$v+($v*0,1),;
+else if ($v1 == 1){
+    echo "Valor da compra: ",$v, "<br>Quantidade de parcelas: ",$v1, "<br>Valor total da compra: ".$v-($v*0.05);
+    for($cont=1;$cont <= $v1; $cont++){
+        if($cont%2==0){
+            $class="clara";
+           }
+           else{
+            $class="escura";
+           }
+           $vtotal=$v+($v*0.05);
+            echo "<tr class=",$class,"><td>",($v-($v*0.05))/$v1,"</td></tr>"; 		   
+         }
     }
-    for($cont=1;$cont<=$v1;$cont++){
-    echo "<tr class=",$class,"<td><b>Valor das parcelas:",$v1"</b></td></tr>";}
+
+   
 
 ?>
 </table>
