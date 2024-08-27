676f4b2c56086eddcaed1779ffe14c4cdb5b0cac
Yago Pereira <yagopereiranunes6@gmail.com>
2024-08-27 10:04:11 -0300

    Create recebe.php

---
+++ aula03apw-27-08-24/atividade/recebe.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<center>
+    <h1>
+        <?php
+        
+        Function calcular($n1, $n2){
+            
+        }
+        //Chamando a funcao
+calcular($n1 = $_POST ["n1"], $n2 = $_POST ["n2"])
+        ?>
+        <br>
+        <a href="atividade.php"> voltar </a> 
+    </h1>
+</body>
+</html>
