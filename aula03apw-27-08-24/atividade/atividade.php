224a08f246c44c93bfd16339f9f1077f081968fd
Yago Pereira <yagopereiranunes6@gmail.com>
2024-08-27 10:03:52 -0300

    Create atividade.php

---
+++ aula03apw-27-08-24/atividade/atividade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nomeidade</title>
+</head>
+<body>
+<form method="post" action="recebe.php">
+        <center>
+            <fieldset>
+                <h3>Insira o nome e idade de uma pessoa:</h3>
+                Nome: <input type="text" name="n1"   ><br><br>
+                Idade: <input type="text"  name="n2"><br><br>
+                <button type="submit"   class="btn btn-primary" name="acao" >Enviar</button>
+                <?php
+                
+                function enviar(){
+                }
+                ?>
+            </fieldset>
+        </center>
+    </form>
+</body>
+</html>
