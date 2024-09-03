55900fa4ada3181f9ef24dab764829f2de6450f9
Yago Pereira <yagopereiranunes6@gmail.com>
2024-09-03 10:54:47 -0300

    Create form.php

---
+++ aula04apw-03-09-24/form.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <title>Calculadora</title>
+</head>
+<body>
+    <form method="post" action="desconto.php">
+        <center>
+            <fieldset>
+                <h3>Número 1:</h3>
+                Número 2: <input type="text" name="n1"   ><br><br>
+                Quantidade de parcelas: <input type="text"  name="n2"><br><br>
+                <button type="submit"   class="btn btn-primary" name="acao" >Enviar</button>
+            </fieldset>
+        </center>
+    </form>
+</body>
+</html>
